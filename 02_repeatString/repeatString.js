5ca1796140f5d160617447b7c62a4a8a51da4d09
Piter <61157319+PiterDev@users.noreply.github.com>
2023-07-20 10:58:20 +0200

    Solve exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,13 @@
-const repeatString = function() {
+const repeatString = function(string, amount) {
+    if (amount < 0) {
+        return "ERROR";
+    }
 
+    let outString = "";
+    for (let i = 0; i<amount; i++) {
+        outString += string;
+    }
+    return outString;
 };
 
 // Do not edit below this line
