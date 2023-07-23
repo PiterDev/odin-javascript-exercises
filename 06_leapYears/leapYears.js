8fa654b4d6acd26a1e0671a69108c6abd26f96fe
Piter <61157319+PiterDev@users.noreply.github.com>
2023-07-23 09:02:13 +0200

    Solve exercise 06

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,19 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    return (year % 4  === 0) && (year % 100 !== 0 || year % 400 === 0);
 };
+
+// Alternatively
+// if (year % 4 === 0) {
+//     if (year % 100 === 0) {
+//         if (year % 400 === 0) {
+//             return true;
+//         } else {
+//             return false;
+//         }
+//     } 
+//     return true;
+// }
+// return false;
 
 // Do not edit below this line
 module.exports = leapYears;
