31fb32ba1555f36dc348c608b88b17c8f23f74f7
Piter <61157319+PiterDev@users.noreply.github.com>
2023-07-23 08:37:48 +0200

    Solve exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    for (i=1; i<args.length; i++) {
+        let argIndex = array.indexOf(args[i]);
+        if (argIndex != -1) {
+            array.splice(array.indexOf(args[i]), 1);
+        }
+    }
+    return array;
 };
 
 // Do not edit below this line
