c157a35ecb8f63cc462a8ee7948744abcdc68f52
Piter <61157319+PiterDev@users.noreply.github.com>
2023-07-23 08:47:04 +0200

    Solve exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,19 @@
-const sumAll = function() {
+const sumAll = function(from, to) {
+    if (!(typeof from === "number" && typeof to === "number")) return "ERROR";
 
+    if (from > to) [from, to] = [to, from];
+    
+    let sum = 0;
+    for (let i = from; i <= to; i++) {
+        sum += i
+    }
+
+    if (sum >= 0) {
+        return sum;
+    } else {
+        return "ERROR";
+    }
+    
 };
 
 // Do not edit below this line
