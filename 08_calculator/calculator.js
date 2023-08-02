a3706f22b7fc45f3479e9b8b018959a49c8c3ad1
Piter <61157319+PiterDev@users.noreply.github.com>
2023-08-02 07:35:18 +0200

    Solve exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,30 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce((obj, current) => current+obj, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(...nums) {
+  return nums.reduce((obj, current) => current*obj, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
-const factorial = function() {
-	
+const factorial = function(number) {
+	if (number === 0) return 1
+  let factorialNum = 1
+  for (let i = 2; i <= number; i++) {
+    factorialNum *= i;
+  } 
+  return factorialNum;
 };
 
 // Do not edit below this line
