8be84e6f2ba4c122ef98c16f0ffc8a82716b102a
Piter <61157319+PiterDev@users.noreply.github.com>
2023-08-02 08:05:46 +0200

    Solve exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,7 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    if (num === 1 || num === 2) return 1;
+    if (num < 1) return "OOPS"; // Since we start coutning at one we shouldn't allow 0 either
+    return fibonacci(num - 1) + fibonacci(num - 2);
 };
 
 // Do not edit below this line
