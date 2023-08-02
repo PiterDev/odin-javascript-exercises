2b32e9d5f538b807a4fd7c65bd2f1a2b313e72a4
Piter <61157319+PiterDev@users.noreply.github.com>
2023-08-02 08:21:36 +0200

    Solve exercise 11

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,10 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    return people.sort((personA, personB) => {
+        let currentYear = new Date().getFullYear();
+        let ageA = "yearOfDeath" in personA ? personA.yearOfDeath - personA.yearOfBirth : currentYear - personA.yearOfBirth;
+        let ageB = "yearOfDeath" in personB ? personB.yearOfDeath - personB.yearOfBirth : currentYear - personB.yearOfBirth;
+        return ageB-ageA;
+    })[0];
 };
 
 // Do not edit below this line
