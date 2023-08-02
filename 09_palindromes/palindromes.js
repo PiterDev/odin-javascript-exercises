0da6d4f27d470c7548e932e01052b46444a61927
Piter <61157319+PiterDev@users.noreply.github.com>
2023-08-02 07:44:26 +0200

    Solve exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (text) {
+    const allowed = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ1234567890";
+    let cleanText = text.split("").filter((letter) => allowed.includes(letter)).join("").toLowerCase();
+    let cleanTextBackwards = cleanText.split("").reverse().join("");
+    return cleanText === cleanTextBackwards;
 };
 
 // Do not edit below this line
